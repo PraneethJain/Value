43bdfe658e5ade2412d964031a31eb4ce2c8b290
PraneethJain <praneethjain005@gmail.com>
2022-11-09 16:26:08 +0530

    Made interval 5 seconds

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import cv2
-from time import time, sleep
 from skimage.metrics import structural_similarity
+from time import time
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("https://100.70.74.109:8080/video")
 
 saved_frame = None
+last_time = time()
 i = 0
 while True:
     cap.grab()
     ret, current_frame = cap.read()
-    
+
+    if time() - last_time < 5:
+        continue
+
     if saved_frame is None:
         cv2.imwrite(f"slide{i}.png", current_frame)
         saved_frame = current_frame
+        last_time = time()
         i += 1
     else:
-        score, diff = structural_similarity(cv2.cvtColor(saved_frame, cv2.COLOR_BGR2GRAY), cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY), full=True)
-        if score < 0.7:
+        score, diff = structural_similarity(
+            cv2.cvtColor(saved_frame, cv2.COLOR_BGR2GRAY),
+            cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY),
+            full=True,
+        )
+        if score < 0.85:
             cv2.imwrite(f"slide{i}.png", current_frame)
             saved_frame = current_frame
+            last_time = time()
             i += 1
-    sleep(2)
