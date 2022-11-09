2040b0023ce15d71cf0e3a4dc2ac6a1efa19360c
PraneethJain <praneethjain005@gmail.com>
2022-11-09 16:03:59 +0530

    Improved sensitivity

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import cv2
 from time import time, sleep
 from skimage.metrics import structural_similarity
 
-cap = cv2.VideoCapture("https://100.70.74.109:8080/video")
+cap = cv2.VideoCapture(0)
 
 saved_frame = None
 i = 0
 while True:
+    cap.grab()
     ret, current_frame = cap.read()
-    cv2.imshow("Input", current_frame)
     
     if saved_frame is None:
         cv2.imwrite(f"slide{i}.png", current_frame)
@@ -16,15 +16,8 @@ while True:
         i += 1
     else:
         score, diff = structural_similarity(cv2.cvtColor(saved_frame, cv2.COLOR_BGR2GRAY), cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY), full=True)
-        if score < 0.6:
+        if score < 0.7:
             cv2.imwrite(f"slide{i}.png", current_frame)
             saved_frame = current_frame
             i += 1
-
-
-    c = cv2.waitKey(1)
-    if c==27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+    sleep(2)
